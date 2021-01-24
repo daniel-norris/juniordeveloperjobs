15e262d7e6594b47fa5e9bc592c32dfdb8ba6397
Dan Norris <dan.norris@hotmail.com>
2021-01-24 15:03:10 +0000

    JD-3: created company and recruiters models and migrations

---
+++ database/migrations/2021_01_24_143504_create_companies_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('name_registered');
+            $table->string('address_1');
+            $table->string('address_2')->nullable();
+            $table->string('city');
+            $table->string('region')->nullable();
+            $table->string('country');
+            $table->string('postcode');
+            $table->string('email');
+            $table->integer('phone');
+            $table->string('url');
+            $table->string('logo');
+            $table->string('account_type');
+            $table->string('account_status')->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}

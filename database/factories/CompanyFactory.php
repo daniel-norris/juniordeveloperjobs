0c6a7e64afeb4ff788722201308b577ab5087cda
Dan Norris <dan.norris@hotmail.com>
2021-01-24 23:01:42 +0000

    JD-13: created a company factory, seeder and symlinked storage

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+
+class CompanyFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Company::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+        $filepath = storage_path('app/public/images');
+
+        if (!Storage::exists($filepath)) {
+            Storage::makeDirectory($filepath);
+        }
+
+        return [
+            'name' => $this->faker->company(),
+            'name_registered' => $this->faker->company(),
+            'address_1' => $this->faker->secondaryAddress(),
+            'city' => $this->faker->city(),
+            'region' => $this->faker->county(),
+            'country' => $this->faker->country(),
+            'postcode' => $this->faker->postcode(),
+            'email' => $this->faker->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'url' => $this->faker->url(),
+            'logo' => $this->faker->image($filepath, 400, 300, 'test', null, false),
+            'account_type' => 'basic',
+        ];
+    }
+}
--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -3,18 +3,17 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Company;
 
-class DatabaseSeeder extends Seeder
+class CompanySeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            CompanySeeder::class,
-        ]);
+        Company::factory()->count(10)->create();
     }
 }

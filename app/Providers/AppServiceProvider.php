dc629b5ecda7034a547250cadace798f39c3f965
Dan Norris <dan.norris@hotmail.com>
2021-01-30 13:30:28 +0000

    JD-5: implemented search functionality for main page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
+use App\Services\SearchProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(SearchProvider::class, function ($app) {
+            return new SearchProvider;
+        });
     }
 
     /**
@@ -24,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('http');
+        //
     }
 }
